b244450ea84e1409fc285ca3dea2ee38d1b89127
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-01-14 20:53:47 -0300

    resolve conflict in index.css

--- frontend/vehicle-management-app-v2/src/App.tsx
+++ frontend/vehicle-management-app-v2/src/App.tsx
@@ -1,35 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-=======
+
 import './App.css';
 import { Navbar } from './layouts/NavAndFooter/NavBar';
 import DriverList from './layouts/Drivers/DriverList';
->>>>>>> frontend
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
+
         <Navbar/>
         <DriverList/>
->>>>>>> frontend
+    
     </div>
   );
 }
