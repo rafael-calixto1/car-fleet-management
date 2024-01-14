046da8c71a12f3ed77325c93e9f14a6f0fc9841f
calixtorafael-1 <calixxtorafaell@gmail.com>
2024-01-14 17:54:43 -0300

    added DriverModel

---
+++ frontend/vehicle-management-app-v2/src/models/DriverModel.tsx
@@ -0,0 +1,13 @@
+class DriverModel {
+    id: number;
+    name: string;
+    licenseNumber: string;
+
+    constructor(id: number, name: string, licenseNumber: string) {
+        this.id = id;
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+    }
+}
+
+export default DriverModel;
